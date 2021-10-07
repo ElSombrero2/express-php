bed5d4bd785426438a9daaecfb89dd98a6b5ed53
8Pixel <contact8pixel@gmail.com>
2021-10-07 15:23:35 +0300

    Autoloader

--- autoload.php
+++ autoload.php
@@ -1,3 +1,6 @@
 <?php
-
-    
+    function load($classname){
+        $name = str_replace('Express\\', '', $classname);
+        require '..\\express'.$name.'.php'; 
+    }
+    spl_autoload_register('load');
