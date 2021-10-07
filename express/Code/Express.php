f0126a9811ec566919139301c87a91319ac51e87
8Pixel <contact8pixel@gmail.com>
2021-10-07 15:29:11 +0300

    Add methods to express

---
+++ express/Code/Express.php
@@ -0,0 +1,43 @@
+<?php
+class Express{
+    public static function json(){
+
+    }
+
+    public static function urlencoded(){
+
+    }
+
+    public function __construct()
+    {
+        
+    }
+
+    public function get(){
+
+    }
+
+    public function post(){
+
+    }
+
+    public function delete(){
+
+    }
+
+    public function put(){
+
+    }
+
+    public function patch(){
+
+    }
+
+    public function head(){
+
+    }
+
+    public function option(){
+        
+    }
+}
