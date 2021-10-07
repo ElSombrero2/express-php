c94e2089776a4ab30035ea27b0ec57268e24255e
8Pixel <contact8pixel@gmail.com>
2021-10-07 15:54:31 +0300

    Add response

--- express/http/Response.php
+++ express/http/Response.php
@@ -1,1 +1,36 @@
 <?php
+    class Response{
+
+        private $code = 200;
+
+        public function __construct(){}
+
+        public function getStatus(){ return $this->code; }
+
+        public function status(int $code){
+            $this->code = $code;
+            return $this;
+        }
+
+        public function send(string $obj){
+            $this->set('X-Powered-By', 'Express')
+            ->set('Content-Type', 'text/html; charset=utf-8')
+            ->set('Content-Length', strlen($obj));
+            http_response_code($this->code);
+            echo $obj;
+        }
+
+        public function json(object $obj){
+            $response = json_encode($obj);
+            $this->set('X-Powered-By', 'Express')
+            ->set('Content-Type', 'application/json; charset=utf-8')
+            ->set('Content-Length', strlen($response));
+            http_response_code($this->code);
+            echo $response;
+        }
+
+        public function set(string $key, string $value){
+            header($key.': '.$value);
+            return $this;
+        }
+    }
